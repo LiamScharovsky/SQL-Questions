a13daa79921f2f6f44f6ba3fb18f85b03012a33c
LiamScharovsky <99295834+LiamScharovsky@users.noreply.github.com>
2022-05-02 11:26:38 -0500

    Delete whiteBoard.py

--- whiteBoard.py
+++
@@ -1,8 +0,0 @@
-def getCount(s):
-    i = 0
-    for letter in s:
-        if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
-            i = i + 1
-
-    return i
-print(getCount("hello there fine person"))
